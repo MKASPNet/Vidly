7cea46e6379ec69362ba47305b6182b6dc2b8fb7
MKSFinalProject <62482999+MKSFinalProject@users.noreply.github.com>
2020-04-25 17:39:04 +0300

    W3 - CodeFirst Migration

---
+++ Vidly/Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext()
+            : base("name=VidlyConnection")
+        {
+
+        }
+    }
+}
