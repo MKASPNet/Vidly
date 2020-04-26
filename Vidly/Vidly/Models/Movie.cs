6e722c21c6b60964883455cb85129cbcc7296e37
MKSFinalProject <62482999+MKSFinalProject@users.noreply.github.com>
2020-04-26 11:42:07 +0300

    W3 - Update Form

--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -9,13 +9,21 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Required]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
     }
 }
